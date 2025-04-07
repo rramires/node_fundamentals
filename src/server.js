655a2acecce26426e3c40fe190b841cd000d3846
rramires <ricardo@flexbr.com>
2025-04-07 11:10:55 -0300

    Adding explanation about methods.

--- src/server.js
+++ src/server.js
@@ -7,10 +7,29 @@
 // Interesting convention node:internalModule to differentiate node-internal modules from third-party ones
 import http from 'node:http';
 
+// HTTP methods are used to define the action to be performed on a resource.
+
+// GET  - Use to retrieve data from a server
+// POST - Use to send data to a server
+// PUT  - Use to update existing data on a server
+// PATCH - Use to apply partial modifications to a resource
+// DELETE - Use to remove data from a server
+
+// We can have more than one method per route
+// For example:
+// POST /users - Create a new user
+// GET /users - Get a list of users
+// GET /users/:id - Get a specific user
+// PUT /users/:id - Update a specific user
+// DELETE /users/:id - Delete a specific user
+ 
 // Create a server
 const server = http.createServer((req, res) => {
 
-    console.log('Request received');
+    const { method, url } = req;
+    // Log the request method and URL
+    console.log(`Request method: ${method}`);  
+    console.log(`Request URL: ${url}`);
 
     res.end('Hello World!');
 });
